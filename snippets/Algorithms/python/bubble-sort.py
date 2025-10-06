7ccc240d4353ba5e9a3daf8a2ab52eb3847cb388
sealthecode <aarushiseal09@gmail.com>
2025-10-06 23:44:41 +0530

    Update bubble-sort.py

--- snippets/Algorithms/python/bubble-sort.py
+++ snippets/Algorithms/python/bubble-sort.py
@@ -9,8 +9,9 @@ def bubble_sort(arr):
         for j in range(0, n - i - 1):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
 
-# Example
-nums = [64, 34, 25, 12, 22, 11, 90]
-bubble_sort(nums)
-print("Sorted array:", nums)
+# Example use
+myList = [64, 34, 25, 12, 22, 11, 90]
+sortedList = bubble_sort(myList)
+print("Sorted array:", sortedList)
